a047ee81e1cdc7ffbaf23712befa9e1b9033c732
sunchuanwen <gouri123>
2024-03-11 18:30:59 +0800

    config: 开发新版2.0

--- src/template/react/index.tsx
+++ src/template/react/index.tsx
@@ -1,24 +1,24 @@
 import { createRoot } from 'react-dom/client';
 import React, { createElement, FC, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RouteList from './router';
+import RouteList from './routes';
 
 export type RoutePageType = {
-    component?: () => Promise<FC>;
-    routes?: RoutePageType[];
+    component?: () => Promise<{ default: FC }>;
+    children?: RoutePageType[];
     path: string;
 };
 
-const renderChildrenRouteList = (childrenRoutes) => {
+const renderChildrenRouteList = (childrenRoutes: RoutePageType[]) => {
     return childrenRoutes?.map((item) => {
-        const { component, path, routes } = item;
+        const { component, path, children } = item;
 
         return (
             <Route
                 element={<Suspense fallback={'loading'}>{createElement(lazy(component))}</Suspense>}
                 path={path}
             >
-                {renderChildrenRouteList(routes)}
+                {renderChildrenRouteList(children)}
             </Route>
         );
     });
