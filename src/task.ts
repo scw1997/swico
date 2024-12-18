bc028f76ef50dea5b7d08cf06e8d3766bdd80b00
sunchuanwen <gouri123>
2024-12-18 14:12:40 +0800

    fix: 优化compile

--- src/task.ts
+++ src/task.ts
@@ -6,8 +6,13 @@ const copyVueFiles = async () => {
     const targetPath = path.resolve(__dirname, '../dist/src/template/vue/Container.vue');
     await fs.copyFile(filePath, targetPath);
 };
-
+const copyReadMeFiles = async () => {
+    const filePath = path.resolve(__dirname, '../README.md');
+    const targetPath = path.resolve(__dirname, '../dist/README.md');
+    await fs.copyFile(filePath, targetPath);
+};
 const main = () => {
     copyVueFiles();
+    copyReadMeFiles();
 };
 main();
